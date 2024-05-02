04c1941d4cc34d5df2712144e736234ac8fa2908
DY Park <mintzzzzz42@gmail.com>
2024-05-02 15:55:18 +0900

    add scripts

---
+++ Source/MultiplayerGame/MyGameMode.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameMode.h"
+
+AMyGameMode::AMyGameMode()
+{
+	
+}
+
+int AMyGameMode::Foo(int a, int b)
+{
+	
+	return 0;
+}
